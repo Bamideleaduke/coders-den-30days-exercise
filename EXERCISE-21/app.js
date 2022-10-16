faee0e200bdde342454d60ae48faad3be0f6fb80
Bamideleaduke <bamideleaduke@gmail.com>
2022-10-16 11:48:06 +0100

    update

--- EXERCISE-21/app.js
+++ EXERCISE-21/app.js
@@ -1,3 +1,27 @@
+
+// Exercises
+// Exercise: Level 1
+// Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name
+// Get get each of the the paragraph using document.querySelector('#id') and by their id
+// Get all the p as nodeList using document.querySelectorAll(tagname) and by their tag name
+// Loop through the nodeList and get the text content of each paragraph
+// Set a text content to paragraph the fourth paragraph,Fourth Paragraph
+// Set id and class attribute for all the paragraphs using different attribute setting methods
+// Exercise: Level 2
+// Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
+// Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
+// Set text content, id and class to each paragraph
+
+// Exercise: Level 3
+// DOM: Mini project 1
+// Develop the following application, use the following HTML elements to get started with. You will get the same code on starter folder. Apply all the styles and functionality using JavaScript only.
+
+// The year color is changing every 1 second
+// The date and time background color is changing every on seconds
+// Completed challenge has background green
+// Ongoing challenge has background yellow
+// Coming challenges have background red
+
 // body
 const body = document.querySelector("body");
 body.style.display = "grid";
@@ -18,21 +42,20 @@ setInterval(function () {
 
 // title
 const title = document.querySelector("#title");
-let div = "<div>...</div>";
-let dd = "<h4>Yo</h4>";
+let dd = "<h4></h4>";
 title.style.textDecoration = "underline";
-
+title.insertAdjacentHTML("afterend", dd);
 // title
 
 // date
-const date = document.querySelector(".date");
+const date = document.querySelector("h4");
 date.textContent = new Date().toGMTString();
 date.style.backgroundColor = "purple"
 date.style.padding = "0.5rem 1rem"
 date.style.fontSize = "1rem"
 date.style.display = "inline"
 // const hexVal = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-const col = ["red","yellow","orange","green"]
+const col = ["red","yellow","orange","green","purple"]
 
 // changing background color at interval 
 setInterval(function(){
@@ -57,7 +80,7 @@ const challenge = document.querySelectorAll("li");
 challenge.forEach((title, i) => {
     title.style.marginBottom = '2px';
     title.style.padding = '1rem';
-    // 
+    // condition for changing colors
     if (title.classList.contains("completed")) {
       title.style.backgroundColor = 'green'
     } else if(title.classList.contains("onGoing")){
